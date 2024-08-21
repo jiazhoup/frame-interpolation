e303b995f2e23accfc8057d68a203da688f9f676
jiazhoup <91639368+jiazhoup@users.noreply.github.com>
2024-08-21 10:44:40 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'FILM',
+    version = '0.1.0',
+    url = '',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+        # Github Private Repository
+        'ExampleRepo @ git+ssh://git@github.com/example_org/ExampleRepo.git'
+    ]
+)
