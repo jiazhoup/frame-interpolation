9a924ce79f57d895aa4d68d0f892e3965fec44aa
jiazhoup <91639368+jiazhoup@users.noreply.github.com>
2024-08-21 14:37:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'film',
+    name = 'film_net',
     version = '0.1.0',
     url = '',
     description = '',
-    packages = find_packages(include=('frame-interpolation/models/film_net/*',))
+    packages = find_packages(include=('frame-interpolation/models/film_net.*',))
 )
